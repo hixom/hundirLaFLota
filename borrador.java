dd00387aeff271cde94be71432a02750e6dbe4e1
hixom <daniel_95_mz@hotmail.com>
2017-11-28 20:07:56 +0100

    en eclipse

---
+++ borrador.java
@@ -0,0 +1,12 @@
+package hundirLaFlota;
+
+import java.util.Scanner;
+
+public static void editarArray(int[] eje) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Vamos a elegir la ficha que solo ocupa una casilla, procedamos...");
+	System.out.println("introduce valor X");
+	valorXiInt = sc.nextInt();
+	System.out.println("introduce valor y");
+	valorYiInt=sc.nextInt();
+	}
